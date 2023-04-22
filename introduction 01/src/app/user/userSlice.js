72e8e02d09082a08f99e535a55010f504e1d837f
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2023-04-22 10:13:09 +0530

    Add files via upload

--- introduction 01/src/app/user/userSlice.js
+++ introduction 01/src/app/user/userSlice.js
@@ -1,1 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
 
+const userSlice= createSlice({
+    name:"user",
+    initialState:{
+        loading:false,
+        user:[],
+        error:''
+    }
+
+    /// reducer call here
+})
+  export default userSlice.reducer; 
