292c722e88b28e24ad0bdd3d9b312437d8d06d9c
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2023-04-24 01:43:10 +0530

    Add files via upload

--- Fetching Data in Redux Toolkit/src/app/user/userSlice.js
+++ Fetching Data in Redux Toolkit/src/app/user/userSlice.js
@@ -1,1 +1,43 @@
+import { createSlice,  createAsyncThunk   } from "@reduxjs/toolkit";
+import axios from "axios";
 
+
+export const fetchUsers= createAsyncThunk("user/fetchUsers", async()=>{
+    return axios.get("http://localhost:5000/api/reduxuser")
+    .then(res=>res.data);
+
+});
+
+
+const userSlice= createSlice({
+    name:"user",
+    initialState:{
+        loading:false,
+        user:[],
+        error:''
+    },
+
+    /// reducer call here
+    extraReducers:builder=>{
+        builder.addCase(fetchUsers.pending, state=>{
+            state.loading= true
+        });
+
+        builder.addCase(fetchUsers.fulfilled, (state, action)=>{
+            state.loading=false
+            state.user= action.payload
+            state.error=''
+        });
+        builder.addCase(fetchUsers.rejected,(state, action)=>{
+           state.loading= false
+           state.user=[]
+           state.error= action.error.message
+        });
+    }
+
+
+
+    /// end reduce
+    
+})
+  export default userSlice.reducer; 
