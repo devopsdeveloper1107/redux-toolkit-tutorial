6bbdbc209277a23b2182ee6b44cecca4137d4e2d
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2023-04-24 01:45:43 +0530

    Add files via upload

---
+++ Fetching Data in Redux Toolkit/src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import { Routes, Route} from 'react-router-dom';
+import Header from './component/header';
+import Home from './component/home';
+import Userlist from './component/userlist';
+import Adduser from './component/adduser';
+import Footer from './component/footer';
+
+function App() {
+  return (
+    <div className="App">
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/userlist" element={<Userlist/>} />
+      <Route path="/adduser" element={<Adduser/>} />
+
+    </Routes>   
+    
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
