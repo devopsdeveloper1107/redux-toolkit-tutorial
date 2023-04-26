0fa691f8488707c74ce916c8b67600a23e55a56f
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2023-04-26 08:28:32 +0530

    Add files via upload

--- Post User Form Data in Redux Toolkit/src/app/user/userSlice.js
+++ Post User Form Data in Redux Toolkit/src/app/user/userSlice.js
@@ -1,1 +1,79 @@
-test
+import { createSlice,  createAsyncThunk   } from "@reduxjs/toolkit";
+import axios from "axios";
+
+
+export const fetchUsers= createAsyncThunk("user/fetchUsers", async()=>{
+    return axios.get("http://localhost:5000/api/reduxuser")
+    .then(res=>res.data);
+
+});
+
+export const addUser= createAsyncThunk("user/addUser", async(values)=>{
+    return fetch("http://localhost:5000/api/addreduxuser", { method:"POST",
+    headers:{Accept:"application/json", "Content-Type":"application/json"} ,
+    body: JSON.stringify({
+        username: values.username,
+        email: values.email,
+        phone: values.phone,
+        address: values.address
+    })
+
+    }).then((res)=> res.json());
+
+});
+
+
+const userSlice= createSlice({
+    name:"user",
+    initialState:{
+        loading:false,
+        user:[],
+        error:'', 
+        isSuccess:''  
+     },
+
+    /// reducer call here
+    extraReducers:builder=>{
+        builder.addCase(fetchUsers.pending, state=>{
+            state.loading= true
+        });
+
+        builder.addCase(fetchUsers.fulfilled, (state, action)=>{
+            state.loading= false
+            state.user= action.payload
+            state.error=''
+        });
+        builder.addCase(fetchUsers.rejected,(state, action)=>{
+           state.loading= false
+           state.user=[]
+           state.error= action.error.message
+        });
+
+        // add user 
+        builder.addCase(addUser.pending, state=>{
+            state.loading= true
+            state.error= ''
+        });
+        builder.addCase(addUser.fulfilled, (state, action)=>{
+             state.loading=false
+             state.user=[]
+             state.isSuccess=action.payload
+        });
+
+        builder.addCase(addUser.rejected, (state, action)=>{
+            state.loading= false
+            state.user=[]
+            state.error= action.error.message
+
+        });
+
+      
+       
+    }
+
+    /// end reduce
+    
+})
+
+
+  export default userSlice.reducer; 
